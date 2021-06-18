5b55efd7d730cf85845aeddcd94e2664a3d7bebc
y-noda <y-noda@entaworks.co.jp>
2021-06-18 11:26:42 +0900

    add react-context

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer, useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
@@ -40,7 +41,9 @@ const Login = (props) => {
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
     isValid: false
-  })
+  });
+
+  const authCtx = useContext(AuthContext);
 
   const { isValid: emailIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -93,7 +96,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AuthContext = React.createContext({
-  isLoggedIn: false
-});
+  isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {}
+}); 
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedsIn');
+    
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.removeItem('isLoggedsIn');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem('isLoggedsIn', '1');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider 
+      value={{
+        isLoggedIn: isLoggedIn, 
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+      
+      }}>{props.children}
+    </AuthContext.Provider>)
+};
 
 export default AuthContext;
